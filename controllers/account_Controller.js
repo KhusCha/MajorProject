63c2c09452b97f4b36854ad22d348a5aca2e97c0
Avinash Kumar <avinashchandan02@gmail.com>
2023-07-09 17:44:17 +0530

    Third Commit

--- controllers/account_Controller.js
+++ controllers/account_Controller.js
@@ -1,5 +1,10 @@
 
 module.exports.konto = function(req, res){
 
-	return res.send("<h2> You Account registration is successfull</h2>");
+	//return res.send("<h2> You Account registration is successfull</h2>");
+
+	return res.render('konto',{
+
+		title: "Benutzer Konto"
+	});
 }
