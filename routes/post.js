ce64134b09cbe1b703f44dc29991774731fd968e
Avinash Kumar <avinashchandan02@gmail.com>
2023-07-28 17:20:01 +0530

    Applied 2 level check for form i.e unregistered user can't see post form and they can't fiddle with website through creating a form upon inspect

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,10 @@
 const exp = require('express');
 
 const router = exp.Router();
+const passport = require('../config/passport-local-strategy');
 
 const postController = require('../controllers/postController');
 
-router.post('/create', postController.create);
+router.post('/create', passport.checkAuthentication, postController.create);
 
 module.exports = router;
