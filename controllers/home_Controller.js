94495b2d8ac6c669372c5bf4ab638fb32ece64b8
Avinash Kumar <avinashchandan02@gmail.com>
2023-08-06 15:48:59 +0530

    Home Controller code is written in a more cleaner way using async await

--- controllers/home_Controller.js
+++ controllers/home_Controller.js
@@ -4,58 +4,83 @@ const Post = require('../models/post');
 const User = require('../models/users');
 
 // render the home page
-module.exports.home = function(req, res){
+// module.exports.home = function(req, res){
 
-	/* console.log(req.cookies);
-	res.cookie('user_id',22); */
-	// Post.find({}, function(err, posts){
-	// 	return res.render('ejsHome',{
+// 	/* console.log(req.cookies);
+// 	res.cookie('user_id',22); */
+// 	// Post.find({}, function(err, posts){
+// 	// 	return res.render('ejsHome',{
 
-	// 		title: " Codeial Home Page",
-	// 		posts: posts
-	// 	});
+// 	// 		title: " Codeial Home Page",
+// 	// 		posts: posts
+// 	// 	});
 
-	// });
+// 	// });
 
-	//Populate the user of each post
-	Post.find()
-	.populate('user')
-	.populate({
-		path:'comments',
-		populate:{
-			path: 'user'
-		}
-	})
-	.then(function( posts){
+// 	//Populate the user of each post
+// 	Post.find()
+// 	.populate('user')
+// 	.populate({
+// 		path:'comments',
+// 		populate:{
+// 			path: 'user'
+// 		}
+// 	})
+// 	.then(function( posts){
 		
-		User.find({}).then(function(users){
+// 		User.find({}).then(function(users){
 
-			return res.render('ejsHome',{
+// 			return res.render('ejsHome',{
 		
-						title: " Codeial Home Page",
-						posts: posts,
-						allUsers: users
+// 						title: " Codeial Home Page",
+// 						posts: posts,
+// 						allUsers: users
 						
-					});
+// 					});
 		
-		}).catch(function(err){
-		console.log(err);
-		})
+// 		}).catch(function(err){
+// 		console.log(err);
+// 		})
 		
-	}).catch(function(err){
-		console.log(err);
-	});
+// 	}).catch(function(err){
+// 		console.log(err);
+// 	});
 	
-}
+// }
+
+
+// Render the Home Page Using async await, now the code looks cleaner
+
+module.exports.home = async function(req, res){
+				
+		try{
+			let posts = await Post.find({})
+				.populate('user')
+				.populate({
+					path:'comments',
+					populate:{
+						path: 'user'
+					}
+				});
+
+				let users = await User.find({});
+
+				return res.render('ejsHome',{
+			
+					title: " Codeial Home Page",
+					posts: posts,
+					allUsers: users					
+				});
+		}catch(err){
+			console.log('Error Details ', err);
+			return;
+		}			
+}		
 
 
 
-/*
 
 
-		
-
-*/
 
 /*Post.find({})
 	.populate('user')
